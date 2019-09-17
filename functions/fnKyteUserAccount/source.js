88a6fd76d5dc799375128ec13b9343b241ab5130
Fabricio Rocha <fabricio@consultimed.com.br>
2019-09-17 11:44:37 -0300

    aqui

--- functions/fnKyteUserAccount/source.js
+++ functions/fnKyteUserAccount/source.js
@@ -1,4 +1,4 @@
-const moment = require('moment')
+//const moment = require('moment')
 exports = function(arg){
   /*
     Accessing application's values:
@@ -14,6 +14,7 @@ exports = function(arg){
     Try running in the console below.
     
   */
-  console.log('usando o moment 2', moment());
+  //console.log('usando o moment 2', moment());
+  console.log(`aqui`);
   return {arg: arg};
 };
